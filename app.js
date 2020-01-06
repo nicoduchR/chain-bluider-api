f45e96208baa013cbd23dc9b479bc6a32e660a9f
Nicolas Duchemann <nicolas.duchemann@redlab.io>
2020-01-06 09:57:57 +0100

    route creation of genesis done

--- app.js
+++ app.js
@@ -7,32 +7,56 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 app.use(bodyParser.json());
-app.post('/genesis', function (req, res) {
+app.get('/genesis', function (req, res) {
 
-  let chainId = req.body.chainId;
-  let consensus = req.body.consensus;
-  let timestamp = req.body.timestamp;
-  let extraData = req.body.extraData;
-  let gasLimit = req.body.gasLimit;
-  let difficulty = req.body.difficulty;
-  let alloc = req.body.alloc;
+  let chainId = ((req.body.chainId != undefined) ? req.body.chainId : Math.floor(Math.random() * 10000) + 100 );
+  let consensus = ((req.body.consensus != undefined) ? req.body.consensus : 'clique' );
+  let timestamp = ((req.body.timestamp != undefined) ? req.body.timestamp : Date.now() );
+  let extraData = ((req.body.extraData != undefined) ? req.body.extraData : '' );
+  let gasLimit = ((req.body.gasLimit != undefined) ? req.body.gasLimit : "0x47b760" );
+  let difficulty = ((req.body.difficulty != undefined) ? req.body.difficulty : "0x1" );
+  let alloc = ((req.body.alloc != undefined) ? req.body.alloc : "7a54c3375e06f05e07866fa842e51c6811219e0d" );
+  let period = ((req.body.period != undefined) ? req.body.period : 15 );
+  
   let genesis = {
-    "chainId": chainId,
-    "consensus": consensus,
-    "timestamp": timestamp,
+    "config":{
+       "chainId": chainId,
+       "homesteadBlock":0,
+       "eip150Block":0,
+       "eip150Hash":"0x0000000000000000000000000000000000000000000000000000000000000000",
+       "eip155Block":0,
+       "eip158Block":0,
+       "byzantiumBlock":0,
+       "constantinopleBlock":0,
+       "petersburgBlock":0,
+       "clique":{
+          "period": period,
+          "epoch":30000
+       }
+    },
+    "nonce":"0x0",
+    "timestamp": `${timestamp}`,
     "extraData": extraData,
     "gasLimit": gasLimit,
     "difficulty": difficulty,
-    "alloc": alloc
-  }
+    "mixHash":"0x0000000000000000000000000000000000000000000000000000000000000000",
+    "coinbase":"0x0000000000000000000000000000000000000000",
+    "alloc":{
+       "7a54c3375e06f05e07866fa842e51c6811219e0d":{
+          "balance":"0x200000000000000000000000000000000000000000000000000000000000000"
+       },
+    },
+    "number":"0x0",
+    "gasUsed":"0x0",
+    "parentHash":"0x0000000000000000000000000000000000000000000000000000000000000000"
+ }
+
+
   genesis = JSON.stringify(genesis);
-  fs.writeFile('./genesis/genesis_' + chainId + '.json', genesis, (err) => {
-      if (err) throw err;
-      console.log('Genesis Saved');
-  });
-  const file = `${__dirname}/genesis/genesis_${chainId}.json`;
-  res.download(file); 
+  res.setHeader('Content-Type', 'application/json');
+  res.end(genesis);
 })
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
